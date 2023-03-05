8f85126b36e777d171f8c924fdb61fef60094148
ramkigithut <reddithi.krishna@gmail.com>
2023-03-05 14:03:21 +0530

    restructured the script of mongodb sh file

--- mongodb_1.sh
+++ mongodb_1.sh
@@ -4,7 +4,6 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
-systemctl enable mongod
-systemctl start mongod
-
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+systemctl enable mongod
+systemctl restart mongod
